88af8be17f5a2a7793c8302287f2c0a96185fbb1
247321453 <247321453@qq.com>
2014-05-18 16:15:02 +0800

    new

---
+++ DataEditorX/Interface/ICardView.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Acer
+ * 日期: 5月18 星期日
+ * 时间: 16:14
+ * 
+ */
+using System;
+
+namespace DataEditorX.Interface
+{
+    /// <summary>
+    /// Description of ICardView.
+    /// </summary>
+    public class ICardView
+    {
+        public ICardView()
+        {
+        }
+    }
+}
