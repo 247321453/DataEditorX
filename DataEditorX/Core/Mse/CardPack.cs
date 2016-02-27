b13012009f50fbaf9e6ded40cb7d1d76e96616af
shadowfox87 <aiyush87@gmail.com>
2016-02-27 13:22:02 -0500

    update rarity

--- DataEditorX/Core/Mse/CardPack.cs
+++ DataEditorX/Core/Mse/CardPack.cs
@@ -1,66 +1,63 @@
-﻿/*
- * 由SharpDevelop创建。
- * 用户： Hasee
- * 日期: 2016/2/27
- * 时间: 7:55
- * 
- * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
- */
-using System;
-
-namespace DataEditorX.Core
+public string getMseRarity()
 {
-	/// <summary>
-	/// Description of CardPack.
-	/// </summary>
-	public class CardPack
+	if(rarity=null)
+		return "common";
+	rarity=rarity.Trim().toLower();
+	if(rarity.Equals("common") or rarity.Equals("short print"))
 	{
-		public CardPack(long id)
-		{
-			this.card_id=id;
-		}
-		
-		public long card_id{
-			get;
-			private set;
-		}
-		public string pack_id;
-		public string pack_name;
-		public string rarity;
-		public string date;
-		
-		public string getMseRarity(){
-			if(this.rarity==null)
-				return "common";
-			string rarity=this.rarity.Trim().ToLower();
-			if(rarity.Equals("common")){
-				return "common";
-			}
-			if(rarity.Equals("rare")){
-				return "rare";
-			}
-			if(rarity.Equals("super") ||rarity.Equals("super rare")){
-				return "super rare";
-			}
-			if(rarity.Contains("secret")){
-				return "secret rare";
-			}
-			if(rarity.Contains("parallel")){
-				return "parallel rare";
-			}
-			if(rarity.Contains("ultimate")){
-				return "ultimate rare";
-			}
-			if(rarity.Contains("ultra")){
-				return "ultra rare";
-			}
-			if(rarity.Contains("gold")){
-				return "gold tech";
-			}
-			if(rarity.Contains("promo")){
-				return "promo";
-			}
-			return this.rarity.Split('/')[0];
-		}
+		return "common";
 	}
+	if(rarity.Equals("rare") or rarity.Equals("normal rare"))
+	{
+		return "rare";
+	}
+	else if(rarity.Contains("parallel") or rarity.Contains("Kaiba") or rarity.Contains("duel terminal"))
+	{
+		return "parallel rare";
+	}
+	else if(rarity.Contains("super") or rarity.Contains("holofoil"))
+	{
+		return "super rare";
+	}
+	else if(rarity.Contains("ultra"))
+	{
+		return "ultra rare";
+	}
+	else if(rarity.Contains("secret"))
+	{
+		return "secret rare";
+	}
+	else if(rarity.Contains("gold"))
+	{
+		return "gold rare";
+	}
+	else if(rarity.Contains("ultimate"))
+	{
+		return "ultimate rare";
+	}
+	else if(rarity.Contains("prismatic"))
+	{
+		return "prismatic rare";
+	}
+	else if(rarity.Contains("star"))
+	{
+		return "star rare";
+	}
+	else if(rarity.Contains("mosaic"))
+	{
+		return "mosaic rare";
+	}
+	else if(rarity.Contains("platinum"))
+	{
+		return "platinum rare";
+	}
+	else if(rarity.Contains("ghost") or rarity.Contains("holographic"))
+	{
+		return "ghost rare";
+	}
+	else if(rarity.Contains("millenium"))
+	{
+		return "millenium rare";
+	}
+	return this.rarity.Split('/')[0];
 }
