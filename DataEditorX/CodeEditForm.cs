2f77bfae3ca6bae3aa50eea564e6cf9b0e38ffe3
purerosefallen <78877@qq.com>
2019-02-18 21:47:32 +0800

    resize by JoyJ

--- DataEditorX/CodeEditForm.cs
+++ DataEditorX/CodeEditForm.cs
@@ -326,7 +326,7 @@ namespace DataEditorX
             MyMsg.Show(
                 LanguageHelper.GetMsg(LMSG.About) + "\t" + Application.ProductName + "\n"
                 + LanguageHelper.GetMsg(LMSG.Version) + "\t1.1.0.0\n"
-                + LanguageHelper.GetMsg(LMSG.Author) + "\tNanahira");
+                + LanguageHelper.GetMsg(LMSG.Author) + "\t菜菜");
         }
 
         void Menuitem_openClick(object sender, EventArgs e)
